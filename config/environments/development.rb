2dd0646750b10ed59c32cdf376a6ba99e2651cc6
yuntianhu <48864579+yuntianhu@users.noreply.github.com>
2023-08-07 11:35:55 -0400

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # config the host 
+  config.hosts << "env-4885858.us.reclaim.cloud"
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
